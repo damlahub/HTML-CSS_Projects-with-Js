c21a6b77abfc696a043647ebedb99a85b693afbf
DamlaOksel <damlaoksel@hotmail.com>
2023-09-30 12:19:18 +0300

    spering - fixed-header.js yazildi

---
+++ Spering/JavaScripts/fixed-header.js
@@ -0,0 +1,18 @@
+window.onscroll= function (){
+    FixedScroll();
+}
+
+const HEADER=document.querySelector("header");
+let headerDistance=HEADER.offsetTop;
+
+function FixedScroll(){
+    if(window.pageYOffset > headerDistance){
+        HEADER.style.position="fixed";
+        HEADER.style.top="0";
+        HEADER.style.boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)";
+        HEADER.style.animation="dropAnimation .5s ease-out";
+    }else{
+        HEADER.style.position="relative";
+        HEADER.style.boxShadow="none";
+    }
+}
