f68d0050edc2ee1aae18863ae53b0bf46733011e
DamlaOksel <damlaoksel@hotmail.com>
2023-09-30 22:34:42 +0300

    spering slider yapildi

---
+++ Spering/JavaScripts/test-slider.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Swiper('.swiper-container', {
+        autoplay: {
+            delay: 2000,
+        },
+    });
+});
--- Spering/JavaScripts/testimonial.js
+++ Spering/JavaScripts/testimonial.js
@@ -5,19 +5,28 @@ let testimonial_section = document.createElement("section");
 testimonial_h1.innerHTML = `TESTIMONIAL`;
 testimonial_section.classList.add("testimonial-card");
 testimonial_section.innerHTML = `
-<h5>John Hissona</h5>
-                <p>passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by
-                    injected humour, or randomised words which don't look even slightly believable. If youThere are many
-                    variations of passages of Lorem Ipsum available, but the majority have suffered alteration in s
-                </p>
-                <img src="Assets/Images/quote.png" alt="">
-                <div class="test-btns">
-                    <div class="btn">
-                        < </div>
-                            <div class="btn">
-                                >
-                            </div>
-                    </div>
+                <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <section class="opinion swiper-slide">
+                    <h5>John Hissona</h5>
+                    <p>passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by
+                    injected humour, or randomised words
+                    </p>
+                    <img src="Assets/Images/quote.png" alt="">                    </section>
+                    <section class="opinion swiper-slide">
+                    <h5>Damla Öksel</h5>
+                    <p>passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by
+                    </p>
+                    <img src="Assets/Images/quote.png" alt="">                    </section>
+                    <section class="opinion swiper-slide">
+                    <h5>Gizem Ay</h5>
+                    <p>passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by
+                        injected humour, or randomised words which don't look even slightly believable. If youThere are many
+                        variations of passages of Lorem Ipsum available, but the majority have suffered alteration in s
+                    </p>
+                    <img src="Assets/Images/quote.png" alt="">                    </section>
+                </div>
+            </div>
 `;
 TEST.appendChild(testimonial_h1);
 TEST.appendChild(testimonial_section);
