3a9f6c5cb73cb3343a92ae2321bb5c67f45735c7
DamlaOksel <damlaoksel@hotmail.com>
2023-09-30 11:46:59 +0300

    Spering login.js yazildi calisir durumda.

---
+++ Spering/JavaScripts/login.js
@@ -0,0 +1,56 @@
+const LOGIN = document.getElementById("login");
+let header = document.querySelector("header");
+
+LOGIN.addEventListener("click", ShowContent);
+
+function ShowContent(e) {
+    e.preventDefault();
+
+    DisableScroll();
+    CreateElement();
+    Close();
+}
+
+function CreateElement() {
+    let section = document.createElement("section");
+
+    section.classList.add("login-content");
+    section.innerHTML = `
+    <section class="login-panel">
+                    <button id="close">
+                        X
+                    </button>
+                    <h1>LOGIN</h1>
+                    <form>
+                        <input type="email" placeholder="E-Mail">
+                        <input type="password" placeholder="Password">
+                        <button class="red-btn">Login</button>
+                    </form>
+                </section>
+    `;
+    header.appendChild(section);
+}
+
+function Close() {
+    let closeBtn = document.getElementById("close");
+    closeBtn.addEventListener("click", function () {
+        header.removeChild(header.lastElementChild);
+        EnableScroll();
+
+    });
+}
+
+function DisableScroll() { //Kaydırmayı Engelle
+    // Konum değerlerini aldık.
+    let scrollTop = document.documentElement.scrollTop;
+    let scrollLeft = document.documentElement.scrollLeft;
+
+        // Eğer scroll kullanırsa, bir önceki konuma gel.
+        window.onscroll = function () {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+function EnableScroll() { //Kaydırma Engelini Kaldır
+    window.onscroll = function () { };
+}
+
